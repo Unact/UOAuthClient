a34059fb5e6f651ba6eb29d86a3b3358fe313a16
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-06-18 11:09:51 +0400

    _windows_deploy.sql

---
+++ sql/_windows_deploy.sql
@@ -0,0 +1,6 @@
+read "C:\Sites\UOAuthClient\sql\_tables.sql";
+read "C:\Sites\UOAuthClient\sql\account.sql";
+read "C:\Sites\UOAuthClient\sql\roleData.sql";
+read "C:\Sites\UOAuthClient\sql\temporaryTables.sql";
+read "C:\Sites\UOAuthClient\sql\tokenRoles.sql";
+read "C:\Sites\UOAuthClient\sql\UOAuthAuthorize.sql";
