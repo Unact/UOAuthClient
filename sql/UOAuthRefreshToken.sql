0a061e685e616b3c93c86f4359303137f875e3dd
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-07-02 12:48:13 +0400

    uac.UOAuthRefreshToken uses inner SP call if possible

--- sql/UOAuthRefreshToken.sql
+++ sql/UOAuthRefreshToken.sql
@@ -7,10 +7,26 @@ returns xml
 begin
     declare @result xml;
 
-    set @result = util.unactGet(util.getUserOption('uoauthurl')
-                                   + '/token?refresh_token=' + @refreshToken
-                                   + '&client_id=' + @clientId
-                                   + '&client_secret=' + @clientSecret);
+    if exists(select *
+                from sys.sysprocedure p join sys.sysuserperm u on p.creator = u.user_id
+               where p.proc_name = 'token' 
+                 and u.user_name = 'ua') then
+                 
+        set @result = ua.token(
+                                @refreshToken,
+                                null,
+                                @clientId,
+                                @clientSecret
+                               );
+                               
+        set @result = xmlelement('response', @result);
+                 
+    else
+        set @result = util.unactGet(util.getUserOption('uoauthurl')
+                                       + '/token?refresh_token=' + @refreshToken
+                                       + '&client_id=' + @clientId
+                                       + '&client_secret=' + @clientSecret);
+    end if;
     
     return @result;
 end
