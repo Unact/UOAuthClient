c1f67caa7f902163f8d22316818946b3b93cff06
Alexander Levin <alevin.ru@gmail.com>
2014-06-01 14:51:57 +0400

    Formatting

--- sql/UOAuthAuthorize.sql
+++ sql/UOAuthAuthorize.sql
@@ -2,35 +2,34 @@ create or replace function uac.UOAuthAuthorize(
     @code STRING
 ) returns xml
 begin
-    declare @result xml;
+    declare @roles xml;
     declare @id ID;
     
     select roles
-        into @result
+        into @roles
         from uac.token
     where token = @code
         and expireTs > now()
     ;
     
     if isnull(@code,'') = '' then
-        set @result = xmlelement('error', 'Token required');
-        return @result;
+        set @roles = xmlelement('error', 'Token required');
+        return @roles;
     end if;
     
-    if @result is null then
-    
-    
+    if @roles is null then
+        
         if isnull(util.getUserOption('uac.emailAuth'),'0') = '0' then    
-            set @result = util.unactGet(
+            set @roles = util.unactGet(
                 util.getUserOption('uoauthurl')
                 + '/roles?access_token='+@code
             );
         else
-            set @result = eac.authorize(@code);
+            set @roles = eac.authorize(@code);
         end if;
             
         if exists(
-            select * from openxml(@result,'/*:response/*:roles/*:role') with(
+            select * from openxml(@roles,'/*:response/*:roles/*:role') with(
                 code long varchar '*:code'
             ) where code = 'authenticated'
         ) then
@@ -45,13 +44,13 @@ begin
                     where token = @code
                 ) as id,
                 @code as token,
-                @result as roles,
+                @roles as roles,
                 (select dateadd(second, isnull(expiresIn, 3600), now())
-                    from openxml(@result,'/*:response/*:token')
+                    from openxml(@roles,'/*:response/*:token')
                         with(ts datetime '*:ts', expiresIn integer '*:expiresIn')
                 ) as expireTs,
                 (select id
-                    from openxml(@result, '/*:response/*:account')
+                    from openxml(@roles, '/*:response/*:account')
                         with(id long varchar '*:id')
                 ) as account
             ;
@@ -60,17 +59,17 @@ begin
             
             insert into uac.tokenRole with auto name select
                 @id as token, code, data
-            from openxml(@result, '/*:response/*:roles/*:role') with(
+            from openxml(@roles, '/*:response/*:roles/*:role') with(
                 code long varchar '*:code', data long varchar '*:data'
             );
             
-            insert into uac.account on existing update with auto name       
+            insert into uac.account on existing update with auto name
             select
                 id,
                 isnull(name, username) as name,
                 email,
                 isnull(code,email) as code
-            from openxml(@result, '/*:response/*:account')
+            from openxml(@roles, '/*:response/*:account')
                 with(
                      id long varchar '*:id',
                      name long varchar '*:name',
@@ -84,6 +83,6 @@ begin
         
     end if;
     
-    return @result;
+    return @roles;
 
 end;
